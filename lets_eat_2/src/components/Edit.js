e2a68bc8812620e5a71175a93b2d22a0ce70621f
nnvar <ubbercube4@gmail.com>
2024-05-18 00:13:03 -0500

    styles
    styled

--- lets_eat_2/src/components/Edit.js
+++ lets_eat_2/src/components/Edit.js
@@ -2,19 +2,18 @@ import React, {useState} from 'react'
 
 export const EditWhereto = ({editWhereto, task}) => {
     const [value, setValue] = useState(task.task);
+    
+    const handleSubmit = (e) => {
+        // prevent default action
+          e.preventDefault();
+          // edit todo
+          editWhereto(value, task.id);
+        };
 
-    const handleSubmit = e => {
-        e.preventDefault();
-
-        editWhereto(value, task.id);
-
-        setValue("")
-        
-    };
   return (
     <form className='WhereTo' onSubmit={handleSubmit}>
         <input type="text" className="whereto-input" value={value} placeholder='Update' onChange={(e) => setValue(e.target.value)}></input>
-        <button type='submit' className='whereto-btn'>Update</button>
+        <button type="submit" className='whereto-btn'>Update</button>
     </form>
   )
 }
